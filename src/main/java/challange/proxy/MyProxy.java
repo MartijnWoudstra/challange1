869a0041abe427d27daf7962d25096e7208d5d47
Lucas <l.j.w.devries@gmail.com>
2016-02-04 12:45:14 +0100

    sidediff

--- src/main/java/challange/proxy/MyProxy.java
+++ src/main/java/challange/proxy/MyProxy.java
@@ -6,6 +6,10 @@ import java.util.*;
 
 public class MyProxy extends PrivacyProxy {
 
+	
+	private String[] blockList = {"analytics", "ads", "advertisement"};
+	
+	
     //////////////////////////////////////////////////////////////////////////
     //
     // Enhance your proxy by implementing the following three methods:
@@ -22,30 +26,38 @@ public class MyProxy extends PrivacyProxy {
 
         // let's simply print the requested URL, for a start that's enough:
         log("Request for: " + url);
-
+        
         // if we want to print all the request headers , use the below code:
         // it does a for-loop over all headers
-/*
+        
         for (String header : requestHeaders.keySet()) {
             // within the for loop, the variable  header  contains the name of the header
             // and you can ask for the contents of that header using requestHeaders.get() .
             log("  REQ: " + header + ": " + requestHeaders.get(header));
         }
-*/
+        
         // example code to do something if a certain requestheader is present:
-/*
-        if (requestHeaders.containsKey("MyHeader")) ........
-*/
 
-        // example code to remove the  Creepyness  header:
-/*
-        requestHeaders.remove("Creepyness");
-*/
+       // if (requestHeaders.containsKey("MyHeader")) {
 
-        // example code to insert (or replace) the  Niceness  header:
-/*
-        requestHeaders.put("Niceness","high");
-*/
+	        // example code to remove the  Creepyness  header:
+	
+	        requestHeaders.remove("User-Agent");
+	        requestHeaders.remove("Accept-Encoding");
+	        requestHeaders.remove("Referer");
+	        requestHeaders.remove("Cookie");
+	        requestHeaders.remove("Cache-Control");
+	        requestHeaders.remove("Pragma");
+	        // example code to insert (or replace) the  Niceness  header:
+	
+	        // requestHeaders.put("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36");
+	        requestHeaders.put("Accept-Encoding", "delfate");
+      //  }
+        if (url.contains("syndication") || url.contains("googleads") || 
+        		url.contains("scorecardresearch") || url.contains("iperceptions") || 
+        		url.contains("optimizely") || url.contains("effectivemeasure")) {
+			return null;
+		}
         // return the (manipulated) headers, or
         return requestHeaders;
 
@@ -62,24 +74,25 @@ public class MyProxy extends PrivacyProxy {
         log("Response: "+httpresponse);
 
         // if you want to (safely, i.e., without binary garbage) print the entire response, uncomment the following:
-/*
-        printSafe(originalBytes);
-*/
+
+        //System.out.println(new String(originalBytes));
+        
+        //printSafe(originalBytes);
+
 
         // if you want to modify the response, you can either modify the byte array directly,
         // or first convert it to a string and then modify that, _if_ you know for sure the response is in text form
         // (otherwise, a string doesn't make sense).
-/*
+        /*
         if (responseHeaders.containsKey("Content-Type") && responseHeaders.get("Content-Type").startsWith("text/html")) {
              String s = new String(originalBytes);
              String s2 = s.replaceAll("headers", " // if you want to (safely, i.e., without binary garbage) print the entire response, uncomment the following: // printSafe(originalBytes); // if you want to modify the response, you can either modify the byte array directly, // or first convert it to a string and then modify that, _if_ you know for sure the response is in text form // (otherwise, a string doesn't make sense). jaja");
              byte [] alteredBytes = s2.getBytes();
              log("L: "+originalBytes.length);
-             responseLength = s2.length();
+             //responseLength = s2.length();
              log("L: "+alteredBytes.length);
              return alteredBytes;
-        }
-*/
+        }*/
         // return the original, unmodified array:
         return originalBytes;
     }
